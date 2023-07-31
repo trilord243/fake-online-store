87678f587001adb8068fd78265c67459a494177d
Felipe Escalona <escalonaf12@gmail.com>
2023-07-31 16:06:14 -0400

    creating cards :tada:

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../Components/Layout'
 import NavBar from '../../Components/Navbar'
+import Card from '../../Components/Card/INDEX.JSX'
 
 function Home() {
     return (
         <Layout>
             <NavBar />
+            <Card />
         </Layout>
     )
 }
