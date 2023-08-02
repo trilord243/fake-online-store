cbd95740516593f07ffaf32d5f92536faabdf00b
Felipe Escalona <escalonaf12@gmail.com>
2023-08-02 17:39:01 -0400

    vite config new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/online-shop/",
+  base: "/fake-online-store/",
   plugins: [react()],
 });
